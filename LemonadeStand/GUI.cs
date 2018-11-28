fd542bfbce1a87eccab837b51558645e2e8d56a3
Jacob Taylor <jacobtaylor727@outlook.com>
2018-11-28 13:37:25 -0600

    Initializing project. Added all the classes I will be using along with member variables needed for those classes.

---
+++ LemonadeStand/GUI.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class GUI : Game
+    {
+        string[] startReportUI; // report at the start of a day
+        string[] storeUI; // store interface
+        string[] endReportUI; // end of the day report (game over will be a cw after this!)
+    }
+}
---
+++ LemonadeStand/Weather.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class Weather : Game
+    {
+        string forcastWeather; // weather is the type of day (ex. Sunny, Cloudy, Windy)
+        string actualWeather;
+        int forcastTemperature; // Temperature refers to the actual degree it is that day
+        int actualTemperature;
+    }
+}
